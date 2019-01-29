e228c84c1221e99eb73b5d27b1e9fedfd3c98e05
Lander Van den Bulcke <landervdb@inuits.eu>
2019-01-29 22:52:04 +0100

    Add boilerplate Prometheus instrumentation
    Started adding actual Prometheus instrumentation, starting with general
    boilerplate code. No specific Jenkins metrics as of yet.
    
    Signed-off-by: Lander Van den Bulcke <landervdb@inuits.eu>

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/landervdb/jenkins_exporter
+
+require (
+	github.com/prometheus/client_golang v0.9.1
+	github.com/prometheus/common v0.2.0
+)
--- main.go
+++ main.go
@@ -14,22 +14,96 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"net/http"
+	"os"
+	"sync"
 
-	"github.com/landervdb/jenkins_exporter/jenkins"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/version"
 )
+
+var (
+	bind        = flag.String("metrics.bind", ":9506", "Address to expose the metrics on")
+	path        = flag.String("metrics.path", "/metrics", "Path to expose the metrics on")
+	jenkinsPath = flag.String("jenkins.path", "/var/lib/jenkins", "Path to the Jenkins folder")
+	logLevel    = flag.String("log.level", "INFO", "The minimal log level to be displayed")
+)
+
+const (
+	namespace = "jenkins"
+)
+
+// Collector is a Prometheus Collector that fetches and generates the Jenkins metrics.
+type Collector struct {
+	path   string
+	mutex  sync.Mutex
+	client *http.Client
+
+	up *prometheus.Desc
+}
+
+// NewCollector creates an instance of Collector.
+func NewCollector(path string) *Collector {
+	return &Collector{
+		path: path,
+		up: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "up"),
+			"Could the Jenkins folder be parsed",
+			nil,
+			nil),
+	}
+}
+
+// Describe sends the descriptors of the metrics provided by this Collector to the provided channel.
+func (c *Collector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.up
+}
+
+// Collect actually collects all the metrics provided by this Collector and sends them to the provided channel.
+func (c *Collector) Collect(ch chan<- prometheus.Metric) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	var up float64 = 1
+
+	_, err := os.Stat(c.path)
+	if err != nil {
+		up = 0
+		log.Errorf("Failed to parse contents of %s", c.path)
+	}
+
+	ch <- prometheus.MustNewConstMetric(c.up, prometheus.GaugeValue, up)
+}
 
 func main() {
 
-	jobPaths := make(chan jenkins.JobPath)
-	go jenkins.GetJobPaths("/tmp/jenkins", jobPaths)
+	flag.Parse()
 
-	for jobPath := range jobPaths {
-		job, err := jobPath.Parse()
-		if err != nil {
-			continue
-		}
-		log.Printf("result: %s number: %d job: %s folder: %s", job.LastBuild.Result, job.LastBuild.Number, job.Name, job.Folder)
+	log.Base().SetLevel(*logLevel)
+	log.Infoln("Starting Jenkins exporter", version.Info())
+	log.Infoln("Build context", version.BuildContext())
+
+	collector := NewCollector(*jenkinsPath)
+	prometheus.MustRegister(collector)
+	prometheus.MustRegister(version.NewCollector("jenkins_exporter"))
+
+	http.Handle(*path, promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+			 <head><title>Jenkins Exporter</title></head>
+			 <body>
+			 <h1>Jenkins Exporter</h1>
+			 <p><a href='` + *path + `'>Metrics</a></p>
+			 </body>
+			 </html>`))
+	})
+
+	log.Infof("Listening on %s", *bind)
+	if err := http.ListenAndServe(*bind, nil); err != nil {
+		log.Fatal(err)
 	}
 
 }
